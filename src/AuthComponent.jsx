9af8cd966c957ff323375d58faecfd628b3d4508
anmoljain16 <75519593+anmoljain16@users.noreply.github.com>
2023-06-27 10:47:00 +0530

    Add files via upload

---
+++ src/AuthComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AuthComponent() {
+    return (
+        <div>AuthComponent, needs auth from backend to access data</div>
+    )
+}
+
+export default AuthComponent
---
+++ src/FreeComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FreeComponent() {
+    return (
+        <div>It is free component does not need any access</div>
+    )
+}
+
+export default FreeComponent
